c78b46be0efe8ba4163ef199680355c7cb8e2be6
Janmeet Grewal <jsg42@sfu.ca>
2026-06-14 11:22:51 -0700

    Lab4 complete

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-#define EXTRA_SIZE 256;
-#define BLOCK_SIZE 128;
-#define BUF_SIZE 100;
+#define EXTRA_SIZE 256
+#define BLOCK_SIZE 128
+#define BUF_SIZE 100
 
 struct header {
   uint64_t size;
@@ -30,13 +29,38 @@ void print_out(char *format, void *data, size_t data_size) {
 
 int main() {
   void *new_space = sbrk(EXTRA_SIZE);
-
   struct header *first_block = (struct header *)new_space;
   struct header *second_block = (struct header *)((char *)new_space + BLOCK_SIZE);
 
   first_block->size = BLOCK_SIZE;
   first_block->next = NULL;
-
   second_block->size = BLOCK_SIZE;
-  second_block - next = first_block;
+  second_block->next = first_block;
+
+  char *first_data = (char *)first_block + sizeof(struct header);
+  char *second_data = (char *)second_block + sizeof(struct header);
+  int data_size = BLOCK_SIZE - sizeof(struct header);
+
+  memset(first_data, 0, data_size);
+  memset(second_data, 1, data_size);
+
+  print_out("First block: %p\n", &first_block, sizeof(first_block));
+  print_out("Second_block: %p\n", &second_block, sizeof(second_block));
+
+  print_out("First block size: %lu\n", &first_block->size, sizeof(first_block->size));
+  print_out("First block next: %p\n", &first_block->next, sizeof(first_block->next));
+
+  print_out("Second block size: %lu\n", &second_block->size, sizeof(second_block->size));
+  print_out("Second block next: %p\n", &second_block->next, sizeof(second_block->next));
+
+  for (int i = 0; i < data_size; i++) {
+    uint64_t value = first_data[i];
+    print_out("%lu\n", &value, sizeof(value));
+  }
+  for (int i = 0; i < data_size; i++) {
+    uint64_t value = second_data[i];
+    print_out("%lu\n", &value, sizeof(value));
+  }
+
+  return 0;
 }
