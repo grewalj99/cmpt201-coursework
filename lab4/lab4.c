db74c7b33ab48bd2dfd7a3997c56075372a7eb54
Janmeet Grewal <jsg42@sfu.ca>
2026-06-14 01:57:11 -0700

    Lab4 update 1

---
+++ lab4/lab4.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define EXTRA_SIZE 256;
+#define BLOCK_SIZE 128;
+#define BUF_SIZE 100;
+
+struct header {
+  uint64_t size;
+  struct header *next;
+};
+
+void handle_error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+void print_out(char *format, void *data, size_t data_size) {
+  char buf[BUF_SIZE];
+  ssize_t len = snprintf(buf, BUF_SIZE, format,
+                         data_size == sizeof(uint64_t) ? *(uint64_t *)data : *(void **)data);
+  if (len < 0) {
+    handle_error("snprintf");
+  }
+  write(STDOUT_FILENO, buf, len);
+}
+
+int main() {
+  void *new_space = sbrk(EXTRA_SIZE);
+
+  struct header *first_block = (struct header *)new_space;
+  struct header *second_block = (struct header *)((char *)new_space + BLOCK_SIZE);
+
+  first_block->size = BLOCK_SIZE;
+  first_block->next = NULL;
+
+  second_block->size = BLOCK_SIZE;
+  second_block - next = first_block;
+}
